a79001c7467de064df425588979e44bd310264e0
Patrik Drab <patrikdrab75@gmail.com>
2025-05-16 14:37:48 +0200

    refactor: remove SearchTableView and simplify SearchBarViewController
    Remove the custom SearchTableView class and its related setup to simplify the
    search UI. The search table view is no longer added to the view hierarchy,
    reducing complexity and unused code. This cleanup improves maintainability
    and prepares the controller for a new or different search implementation.

--- MHD/Screens/SearchBarView/SearchBarViewController.swift
+++ MHD/Screens/SearchBarView/SearchBarViewController.swift
@@ -10,91 +10,13 @@ import UIKit
 import UIKitTools
 
 
-class SearchTableView: UIView {
-    private var tableView: UITableView!
-    private var searchController: UISearchController!
-    
-    
-    private var dataSource: [String] = ["Auto", "Jablko", "Lietadlo", "Porshe", "Tomas", "Presov", "Website", "Swift", "UIKit"] // Example data source
-    private var filteredData: [String] = [] // For search results
-    
-    init() {
-        super.init(frame: .zero)
-        backgroundColor = .systemBackground
-        
-        setupTable()
-        setupSearchBar()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupTable() {
-        tableView = UITableView()
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.reuseIdentifier)
-        
-        tableView.contentInsetAdjustmentBehavior = .never
-        
-        addSubview(tableView)
-        tableView.pinToSuperviewSafeAreaLayoutGuide()
-    }
-    
-    private func setupSearchBar() {
-        searchController = UISearchController(searchResultsController: nil)
-        searchController.searchResultsUpdater = self
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.hidesNavigationBarDuringPresentation = false
-        searchController.searchBar.placeholder = "Vyhľadať"
-        
-        tableView.tableHeaderView = searchController.searchBar
-    }
-}
-
-extension SearchTableView: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchController.isActive ? filteredData.count : dataSource.count
-    }
-    
-    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.reuseIdentifier, for: indexPath)
-        
-        let data = searchController.isActive ? filteredData[indexPath.row] : dataSource[indexPath.row]
-    
-        cell.textLabel?.text = data
-        return cell
-    }
-}
-
-extension SearchTableView: UISearchResultsUpdating {
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let searchText = searchController.searchBar.text else { return }
-        
-        filteredData = dataSource.filter { item in
-            return item.lowercased().contains(searchText.lowercased())
-        }
-        
-        tableView.reloadData()
-    }
-}
-
-
-
-
 class SearchBarViewController: UIViewController {
     
-    private let searchTableView = SearchTableView()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        setupSearchTableView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -102,10 +24,7 @@ class SearchBarViewController: UIViewController {
         setupNavigationBar()
     }
     
-    private func setupSearchTableView() {
-        view.addSubview(searchTableView)
-        searchTableView.pinInSuperview()
-    }
+    
     
     private func setupNavigationBar() {
         if let navController = navigationController as? NavigationController {
