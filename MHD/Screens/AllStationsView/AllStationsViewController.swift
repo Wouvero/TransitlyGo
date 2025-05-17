34b05daf47462613edcfab0d4098908fa66d50e1
Patrik Drab <patrikdrab75@gmail.com>
2025-05-17 16:04:58 +0200

    feat: add AllStationsViewController to project
    Add a new AllStationsViewController as a placeholder for the all stations
    screen. Update the Xcode project to include the new view controller and its
    group. This lays the groundwork for implementing the all stations feature.

---
+++ MHD/Screens/AllStationsView/AllStationsViewController.swift
@@ -0,0 +1,15 @@
+//
+//
+//
+// Created by: Patrik Drab on 17/05/2025
+// Copyright (c) 2025 MHD 
+//
+//         
+
+import UIKit
+
+class AllStationsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
