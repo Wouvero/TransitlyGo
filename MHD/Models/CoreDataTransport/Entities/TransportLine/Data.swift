7293571c0b5696e2178f063ffdd0e720e80684f9
Patrik Drab <patrikdrab75@gmail.com>
2025-05-20 16:19:53 +0200

    feat(Data & CDTransportLine+DommyData):
    Modify dummy data

---
+++ MHD/Models/CoreDataTransport/Entities/TransportLine/Data.swift
@@ -0,0 +1,40 @@
+//
+//
+//
+// Created by: Patrik Drab on 19/05/2025
+// Copyright (c) 2025 MHD 
+//
+//
+
+
+let transportLines = [
+    (indexPosition: Int64(0), id: "TS_4", name: "4"),
+]
+
+let stationInfoData = [
+    (id: "STI_2", stationName: "Sidlisko III"),
+    (id: "STI_3", stationName: "Prostejovská"),
+    (id: "STI_4", stationName: "Centrum"),
+    (id: "STI_5", stationName: "VUKOV"),
+    (id: "STI_6", stationName: "Námestie Královnej pokoja"),
+    (id: "STI_7", stationName: "Volgogradská"),
+    (id: "STI_8", stationName: "Clementisova"),
+    (id: "STI_9", stationName: "Poliklinika"),
+    (id: "STI_10", stationName: "Trojica"),
+    (id: "STI_11", stationName: "Na Hlavnej"),
+    (id: "STI_12", stationName: "Velká pošta"),
+    (id: "STI_13", stationName: "Cierny most"),
+    (id: "STI_14", stationName: "Železničná stanica"),
+    (id: "STI_15", stationName: "Nový Solivar"),
+    (id: "STI_16", stationName: "Košická"),
+    (id: "STI_17", stationName: "Chalupkova"),
+    (id: "STI_18", stationName: "Svábska"),
+    (id: "STI_19", stationName: "Lomnická"),
+    (id: "STI_20", stationName: "Škára"),
+    (id: "STI_21", stationName: "Lesnícka"),
+    (id: "STI_22", stationName: "Pavla Horova"),
+    (id: "STI_23", stationName: "Martina Benku"),
+    (id: "STI_24", stationName: "Laca Novomeského"),
+    (id: "STI_25", stationName: "Vansovej"),
+    (id: "STI_26", stationName: "Pod Salgovíkom")
+]
