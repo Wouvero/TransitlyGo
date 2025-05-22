96e09107e17dd113ff7135cc5e9b0caaa44651e3
Patrik Drab <patrikdrab75@gmail.com>
2025-05-22 09:38:56 +0200

    feat(UI): Set default overrideUserInterfaceStyle to .light

--- MHD/App/SceneDelegate.swift
+++ MHD/App/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         
         window.rootViewController = TabBarController(tabBatItems: TabBarItem.tabBarItems)
-        
+        window.overrideUserInterfaceStyle = .light
         //generateDummyData()
         self.window = window
         window.makeKeyAndVisible()
