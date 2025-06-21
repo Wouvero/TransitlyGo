c8414b1cad9f10ffa92f59a2b374c502faffb137
Patrik Drab <patrikdrab75@gmail.com>
2025-06-22 00:01:30 +0200

    feat: add favorite button to ResultScreenViewController
    Refactor date and time formatting to use formatted properties from
    searchRouteModel for consistency. Add a circular favorite button that
    allows users to add the current route to favorites. Implement logic to
    check if the route already exists in favorites and update the button
    state accordingly. Prepare for future UI feedback when adding an
    existing favorite. This improves user interaction by enabling quick
    route favoriting.

--- MHD/Screens/C/RouteSelectionScreen/ResultScreen/ResultScreenViewController.swift
+++ MHD/Screens/C/RouteSelectionScreen/ResultScreen/ResultScreenViewController.swift
@@ -12,24 +12,35 @@ class ResultScreenViewController: UIViewController, MHD_NavigationDelegate {
     var contentLabelText: NSAttributedString {
         let fromStationName = searchRouteModel.fromStationInfo.stationName ?? ""
         let toStationName = searchRouteModel.toStationInfo.stationName ?? ""
-        let dateSelection = formatDate(searchRouteModel.selectedDate)
-        let time = searchRouteModel.hour + ":" + searchRouteModel.minute
+        let selectedDay = searchRouteModel.formattedSelectedDay
+        let time = searchRouteModel.formattedTime
         
         return NSAttributedStringBuilder()
             .add(text: "Zo zastávky ", attributes: [.font: UIFont.interRegular(size: 16)])
             .add(text: "\(fromStationName) ", attributes: [.font: UIFont.interSemibold(size: 16)])
             .add(text: "na ", attributes: [.font: UIFont.interRegular(size: 16)])
             .add(text: "\(toStationName) ", attributes: [.font: UIFont.interSemibold(size: 16)])
-            .add(text: "\(dateSelection) od ", attributes: [.font: UIFont.interRegular(size: 16)])
+            .add(text: "\(selectedDay) od ", attributes: [.font: UIFont.interRegular(size: 16)])
             .add(text: "\(time) ", attributes: [.font: UIFont.interSemibold(size: 16)])
             .build()
     }
     
+    private let addToFavoriteBtn = CustomButton.makeCircleBtn(
+        iconName: "heart",
+        iconColor: .white,
+        iconSize: 24,
+        backgroundColor: .systemBlue,
+        size: 50
+    )
+    
     let searchRouteModel: SearchRouteModel
+    
+    var itemExists: Bool = false
     
     init(searchRouteModel: SearchRouteModel) {
         self.searchRouteModel = searchRouteModel
         super.init(nibName: nil, bundle: nil)
+        self.itemExists = checkIfItemExists()
     }
     
     required init?(coder: NSCoder) {
@@ -39,5 +50,47 @@ class ResultScreenViewController: UIViewController, MHD_NavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .neutral10
+        setFavoriteBtn()
+    }
+    
+    private func checkIfItemExists() -> Bool {
+        guard let fromStationName = searchRouteModel.fromStationInfo.stationName,
+              let toStationName = searchRouteModel.toStationInfo.stationName else { return false }
+        
+        let context = MHD_CoreDataManager.shared.viewContext
+        return MHD_Favorite.checkIfItemExists(
+            for: fromStationName,
+            and: toStationName,
+            in: context
+        )
+    }
+    
+    private func setFavoriteBtn() {
+
+        addToFavoriteBtn.onRelease = { [weak self] in
+            guard let self else { return }
+            
+            let context = MHD_CoreDataManager.shared.viewContext
+            
+            if !itemExists {
+                MHD_Favorite.addToFavorite(searchRouteModel, in: context)
+                itemExists = true
+                addToFavoriteBtn.setButtonIcon("heart.fill")
+            } else {
+                print("Item exist.")
+                
+                // TODO
+                // implement pop information about item existing.
+            }
+        }
+        addToFavoriteBtn.setButtonIcon(itemExists ? "heart.fill" : "heart")
+        
+        view.addSubview(addToFavoriteBtn)
+        addToFavoriteBtn.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            addToFavoriteBtn.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            addToFavoriteBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
     }
 }
